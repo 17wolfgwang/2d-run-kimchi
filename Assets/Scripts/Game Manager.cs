5fa9f0faddb179cc9f419a5d487f805bc51addb8
17wolfgwang <dkzks44@gmail.com>
2024-12-29 13:01:02 -0800

    UPDATE: Player & Spawn

---
+++ Assets/Scripts/Game Manager.cs
@@ -0,0 +1,68 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public enum GameState
+{
+    Intro,
+    Playing,
+    Dead
+}
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance;
+    public GameState State = GameState.Intro;
+
+    public int Lives = 3;
+
+    [Header("References")]
+    public GameObject IntroUI;
+
+    public GameObject DeadUI;
+
+    public GameObject EnemySpawner;
+    public GameObject FoodSpawner;
+    public GameObject GoldenSpawner;
+
+    public Player PlayerScript;
+
+
+    void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+    }
+    void Start()
+    {
+        IntroUI.SetActive(true);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (State == GameState.Intro && Input.GetKeyDown(KeyCode.Space))
+        {
+            State = GameState.Playing;
+            IntroUI.SetActive(false);
+            EnemySpawner.SetActive(true);
+            FoodSpawner.SetActive(true);
+            GoldenSpawner.SetActive(true);
+
+        }
+        if (State == GameState.Playing && Lives == 0)
+        {
+            PlayerScript.KillPlayer();
+            EnemySpawner.SetActive(false);
+            FoodSpawner.SetActive(false);
+            GoldenSpawner.SetActive(false);
+            DeadUI.SetActive(true);
+            State = GameState.Dead;
+        }
+        if (State == GameState.Dead && Input.GetKeyDown(KeyCode.Space))
+        {
+            SceneManager.LoadScene("Main");
+        }
+    }
+}
